26e0eec73d87add6f57fbb7466d5e93207ec0c6b
Nadelopo <101577205+Nadelopo@users.noreply.github.com>
2022-10-14 18:57:33 +0400

    firstcommit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <div class="mb-4">1</div>
+    <main><router-view /></main>
+  </div>
+</template>
+
+<style lang="sass">
+@import "tailwindcss/base"
+@import "tailwindcss/components"
+@import "tailwindcss/utilities"
+
+*
+  box-sizing: border-box
+  font-family: Roboto
+</style>
