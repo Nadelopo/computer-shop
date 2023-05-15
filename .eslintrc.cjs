b48a4a361b8bbf96922a85ee3ce2f857ce4d6aff
Nadelopo <mistrmorsikk@mail.ru>
2023-05-16 01:00:17 +0400

    update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,7 +20,6 @@ module.exports = {
     'vue/multi-word-component-names': 'off',
     'vue/max-attributes-per-line': 'off',
     'vue/html-self-closing': 'off',
-    'vue/html-indent': 'off',
-    'vue/no-setup-props-destructure': 'off'
+    'vue/html-indent': 'off'
   }
 }
--- src/components/UI/VInputText.vue
+++ src/components/UI/VInputText.vue
@@ -50,5 +50,26 @@ onMounted(() => {
 </template>
 
 <style scoped lang="sass">
-@import '@/styles/input.sass'
+.wrapper
+  position: relative
+  &::after
+    content: ''
+    width: 100%
+    height: 2px
+    position: absolute
+    bottom: -6px
+    left: 0
+    background: var(--color-main)
+    transition: 0.4s
+    transform: scaleX(0)
+  &:focus-within::after
+    transform: scaleX(1)
+
+.input
+  outline: none
+  width: 100%
+  height: 30px
+  border-bottom: 1px solid #9e9e9e
+  transition: 0.4s
+  background: initial
 </style>
